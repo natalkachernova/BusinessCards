4be4d7e5d7aca72db9903809d0848de4d3948f9a
natalkachernova <120986642+natalkachernova@users.noreply.github.com>
2023-01-24 15:15:11 +0200

    Add files via upload

--- Module_7_Task_1.py
+++ Module_7_Task_1.py
@@ -1,35 +1,45 @@
 from faker import Faker
 fake = Faker()
 
-class Contact_Card:
-   def __init__(self, name, company_name, surname, position, email):
+class ContactCard:
+    def __init__(self,
+                name: str,
+                company_name: str,
+                surname: str,
+                position: str,
+                email: str
+               ) -> None:
         self.name = name
         self.surname = surname
         self.company_name = company_name
         self.position = position
         self.email = email
 
-class BaseContact(Contact_Card):
+    
+    def __str__(self) -> str:
+        return f'{self.name} {self.surname} {self.email}'
+
+
+class BaseContact(ContactCard):
     def __init__(self, name, surname, phone_number, email):
         self.name = name
         self.surname = surname
         self.phone_number = phone_number
         self.email = email
 
-class BusinessContact(Contact_Card):
+    def length_name(self):
+        return len(self.name + ' ' + self.surname)
+  
+    def contact(self):
+        print("Connecting with ...")
+        print(f' {self.name} \n {self.surname} \n {self.email}')
+        print(f'I dial {self.phone_number} and call to {self.name} {self.surname}.')
+
+
+class BusinessContact(ContactCard):
     def __init__(self, company_name, position):
         self.company_name = company_name
         self.position = position
-
-def length_name(self):
-    return len(self.name + ' ' + self.surname)
-  
-def contact(self):
-    print("Connecting with ...")
-    print(f' {self.name} \n {self.surname} \n {self.phone_number} \n {self.email}')
-
-def __str__(self):
-    return f'{self.name} {self.surname} {self.email}'
 
 BaseContacts_Book = []
 BusinessBaseContacts_Book = []
@@ -46,23 +56,23 @@ for j in range(number_cards):
                                                     position = "Occupational social worker"))
 
 for i in range(number_cards):    
-    print(__str__(BaseContacts_Book[i]))
+    print(ContactCard.__str__(BaseContacts_Book[i]))
 print("--------- Sorted by name -------")
 by_name = sorted(BaseContacts_Book, key=lambda Busines_Card: Busines_Card.name)
 for i in range(number_cards):    
-    print(__str__(by_name[i]))
+    print(ContactCard.__str__(by_name[i]))
 print("--------- Sorted by surname ----")
 by_surname = sorted(BaseContacts_Book, key=lambda Busines_Card: Busines_Card.surname)
 for i in range(number_cards):    
-    print(__str__(by_surname[i]))
+    print(ContactCard.__str__(by_surname[i]))
 print("--------- Sorted by email ------")
 by_email = sorted(BaseContacts_Book, key=lambda Busines_Card: Busines_Card.email)
 for i in range(number_cards):    
-    print(__str__(by_email[i]))
-print("--------- Sending email -------")
+    print(ContactCard.__str__(by_email[i]))
+print("--------- Send an email and call -------")
 name_to_send = input("Enter the recipient's name: ")
 for i in range(number_cards):
     if name_to_send == BaseContacts_Book[i].name:
-        contact(BaseContacts_Book[i])
-        print("Legth Name and Surname: ", length_name(BaseContacts_Book[i]))
+        BaseContact.contact(BaseContacts_Book[i])
+        print("Legth Name and Surname: ", BaseContact.length_name(BaseContacts_Book[i]))
         break
